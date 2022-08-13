25d219841a8741ed93d8af04b3fcfedc932d47e2
Dalton Miller <dalton.kye.miller@gmail.com>
2022-08-13 12:50:56 -0500

    added api proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+    plugins: [react()],
+    proxy: {
+        '/api': {
+            target: 'https://localhost:8080',
+            changeOrigin: true,
+            secure: false,
+            ws: true,
+        }
+    }
+});
