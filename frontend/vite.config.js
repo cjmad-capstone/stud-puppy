38c94cbd161ac247bbef40d164507a9b47325b67
Dalton Miller <dalton.kye.miller@gmail.com>
2022-08-13 12:52:22 -0500

    fix: proxy config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,14 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    proxy: {
-        '/api': {
-            target: 'https://localhost:8080',
-            changeOrigin: true,
-            secure: false,
-            ws: true,
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+                secure: false,
+                ws: true,
+            }
         }
     }
 });
