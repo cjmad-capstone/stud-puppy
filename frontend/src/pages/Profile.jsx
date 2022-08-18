e75ea59b092923b3a97602b32b37bfd6075c83e2
Justin Welsh <justinwelsh33@gmail.com>
2022-08-18 12:34:40 -0500

    update styling

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -10,9 +10,9 @@ const Profile = () => {
         <motion.main {...pt}>
             <div>
                 {/*Carousel*/}
-                <div className="carousel w-full">
-                    <div id="slide1" className="carousel-item relative w-full">
-                        <img src="https://placeimg.com/800/200/arch" className="w-full"/>
+                <div className="carousel w-full h-[600px]">
+                    <div id="slide1" className="carousel-item relative w-full h-full ">
+                        <img src="https://www.thestatesman.com/wp-content/uploads/2022/07/AmericanBullysobakabarobaka-4ce0d4dc0e144dccadb5159b222e275e-e1657808052501.jpg" className="w-full h-full object-center object-cover"/>
                         <div
                             className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                             <a href="#slide4" className="btn btn-circle opacity-75">❮</a>
@@ -53,7 +53,7 @@ const Profile = () => {
                             <h1 className="card-title text-5xl">Buddy, 7</h1>
                             <h2 className="text-3xl pb-4">golden retriever</h2>
                             <p>Buddy is energetic and playful and looking for a new friend. He loves running and running and also running!</p>
-                            <div className="card-actions justify-center pt-7">
+                            <div className="card-actions justify-center pt-6">
                                 <Button className={`text-sm w-full hover:from-pink-300 hover:to-red-400`}>Schedule A Meetup</Button>
                             </div>
                         </div>
@@ -64,12 +64,14 @@ const Profile = () => {
                         <div className="card-body">
                             <h2 className="text-xl">Meet the Owner</h2>
                             <div className={`flex items-center`}>
-                                <figure className={`w-1/3 h-6`}><img src="https://placeimg.com/400/225/arch" alt="User pic" className={`rounded-full`} /></figure>
+                                <figure className={`w-16 h-16`}>
+                                    <img src="https://placeimg.com/400/225/arch" alt="User pic" className={`rounded-full w-full h-full object-cover`} />
+                                </figure>
                                 <h1 className="text-2xl pl-4">Lela</h1>
                             </div>
                             <div className={`text-sm`}>
                                 <h6>lela@example.com | 555-5555</h6>
-                                <h6>"I'm a passionate dog owner for over 10 years"</h6>
+                                <h6 className={`py-6`}>"I'm a passionate dog owner for over 10 years"</h6>
                             </div>
                         </div>
                     </div>
