c4d985982aacffccb7bfe3f3d9faaa6c4ffa16ee
Dalton Miller <dalton.kye.miller@gmail.com>
2022-08-18 14:49:36 -0500

    fix: dog card

--- frontend/src/components/DogCard/DogCard.jsx
+++ frontend/src/components/DogCard/DogCard.jsx
@@ -1,19 +1,3 @@
-<<<<<<< Updated upstream
-
-const DogCard = ({ imgPath, name, className, children, ...rest }) => {
-    return (
-        <div className="card w-96 bg-base-100 shadow-xl">
-            <figure><img src="https://placeimg.com/400/225/arch" /*src={imgPath} */ alt="Dog Photo"/></figure>
-            <div className="card-body">
-                <h2 className="card-title">
-                    I'm a Dog!
-                    <div className="badge badge-secondary">NEW</div>
-                </h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div className="card-actions justify-end">
-                    <div className="badge badge-outline">User Profile</div>
-                    <div className="badge badge-outline">Dog Profile</div>
-=======
 function DogCard({ dog }) {
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
@@ -30,17 +14,10 @@ function DogCard({ dog }) {
                 <p>{dog?.description}</p>
                 <div className="card-actions justify-end">
                     <button className="btn btn-primary">Buy Now</button>
->>>>>>> Stashed changes
                 </div>
             </div>
         </div>
     );
-<<<<<<< Updated upstream
-};
-
-export default DogCard;
-=======
 }
 
 export default DogCard;
->>>>>>> Stashed changes
