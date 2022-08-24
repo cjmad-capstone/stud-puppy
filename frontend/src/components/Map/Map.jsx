c1ba2b7586a74965234e05efe03525a0f9d72c61
Justin Welsh <justinwelsh33@gmail.com>
2022-08-23 19:13:59 -0500

    created map component with geocoder functionality

--- frontend/src/components/Map/Map.jsx
+++ frontend/src/components/Map/Map.jsx
@@ -1,10 +1,10 @@
 import React, {useEffect, useRef} from 'react'
 import 'https://api.mapbox.com/mapbox-gl-js/v2.9.2/mapbox-gl.js';
+import 'https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v5.0.0/mapbox-gl-geocoder.min.js';
 import {MAPBOX_TOKEN} from "../../const/secrets.js";
-import './Map.module.css'
+import  './Map.module.css'
 
 const Map = () => {
-    console.log(mapboxgl)
 
     mapboxgl.accessToken = MAPBOX_TOKEN;
 
@@ -12,13 +12,20 @@ const Map = () => {
         const map = new mapboxgl.Map({
             container: 'map', // container ID
             style: 'mapbox://styles/mapbox/streets-v11', // style URL
-            center: [-74.5, 40], // starting position [lng, lat]
+            center: [-73.990593, 40.740121], // starting position [lng, lat]
             zoom: 9, // starting zoom
             projection: 'globe' // display the map as a 3D globe
         });
         map.on('style.load', () => {
             map.setFog({}); // Set the default atmosphere style
         });
+
+        map.addControl(
+            new MapboxGeocoder({
+                accessToken: mapboxgl.accessToken,
+                mapboxgl: mapboxgl
+            })
+        );
     }
 
     useEffect(initMap, [])
