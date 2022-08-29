d003e37b167ce0ebcedc8607dbfe9096056c0462
Alex Rodriguez <alexander.joshua.rodriguez@gmail.com>
2022-08-29 16:14:20 -0500

    updated create event page

--- src/main/java/com/cjmad/capstone/controllers/EventController.java
+++ src/main/java/com/cjmad/capstone/controllers/EventController.java
@@ -1,8 +1,11 @@
 package com.cjmad.capstone.controllers;
 
 import com.cjmad.capstone.models.Event;
+import com.cjmad.capstone.models.User;
 import com.cjmad.capstone.repositories.EventsRepository;
 import com.cjmad.capstone.repositories.UserRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,9 +15,11 @@ import java.util.List;
 public class EventController {
 
     private final EventsRepository eventsRepository;
+    private final UserRepository userRepository;
 
     public EventController(EventsRepository eventsRepository, UserRepository userRepository) {
         this.eventsRepository = eventsRepository;
+        this.userRepository = userRepository;
     }
 
     @GetMapping
@@ -23,7 +28,10 @@ public class EventController {
     }
 
     @PostMapping
+    @PreAuthorize("hasRole('ROLE_USER')")
     public Event createEvent(@RequestBody Event event) {
+        User user = userRepository.findByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
+        event.setCreator(user);
         return eventsRepository.save(event);
     }
 
