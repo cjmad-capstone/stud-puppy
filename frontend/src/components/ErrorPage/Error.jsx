fbe2d66e1f98f946d18589edf6a076f054e4d3c1
Cynthia Gallegos <cynthialgallegos97@gmail.com>
2022-08-29 12:20:16 -0500

    sad dog

--- frontend/src/components/ErrorPage/Error.jsx
+++ frontend/src/components/ErrorPage/Error.jsx
@@ -14,15 +14,30 @@ const ErrorPage= () =>{
             title = 'Error 404'
             subtitle = 'Page Not Found'
             break;
+        case 403:
+            title = 'Error 403'
+            subtitle = 'Forbidden'
+            break;
+        case 401:
+            title = 'Error 401'
+            subtitle = 'Unauthorized'
+            break;
+        case 400:
+            title = '400'
+            subtitle = 'Bad Request'
+            break;
+
+        default: errorCode
+
     }
     return (
         <motion.main {...pt} className={`w-full px-4`}>
-            <div className="card w-full h-96 bg-gradient-to-br from-pink-400 to-red-500 text-primary-content">
+            <div className="card w-full h-screen bg-gradient-to-br from-pink-400 to-red-500 text-primary-content">
                 <div className="card-body">
-                    <img className="absolute m-auto right-0 h-[150%] md:h-[110%] top-0" src="img/doge-punching-cheems.png" alt="Shoes"/>
+                    <img className="absolute mt-[60%] h-[50%] right-3 md:mt-0 md:h-[125%] md:top-0 md: right-0 md:mx-auto md:right-0" src="img/thug-life-pug-transparent-background-sad-dog-jpg-1226295.png" alt="Shoes"/>
 
-                    <h2 className="card-title ">{title} </h2>
-                    <p>{subtitle}</p>
+                    <h2 className="card-title text-8xl">{title} </h2>
+                    <p className={"text-3xl"}>{subtitle}</p>
                 </div>
             </div>
         </motion.main>
