50b89a1125ac867edff0ef181ee93ed46c80a590
Alex Rodriguez <alexander.joshua.rodriguez@gmail.com>
2022-08-31 14:49:47 -0500

    update to about page

--- frontend/src/pages/AboutPage.jsx
+++ frontend/src/pages/AboutPage.jsx
@@ -10,13 +10,13 @@ const AboutPage = () => {
 
 
             <h1 className="m-10 text-8xl font-bold">About Us.</h1>
-                        <p className="decoration-8 w-fit mb-5 mt-5 text-center">A full-stack web application that allows you to find your best buddy, a play date. It’s complete with a modern design made using React.js, styled with TailwindCSS, animated with Framer Motion. The user can sign up for an account then get to creating their dog’s profile. It allows dog-owners to collaborate in an effort to help their furry friend live their best life. My team and I were able to design and implement a RESTful API using Spring Boot with full CRUD functionality that communicates with the frontend to authenticate with JWTs.</p>
+            <p className="decoration-8 w-fit mb-5 mt-5 text-center">A full-stack web application that allows you to find your best buddy, a play date. It's complete with a modern design made using React.js, styled with TailwindCSS, animated with Framer Motion. The user can sign up for an account then get to creating their dog's profile. It allows dog-owners to collaborate in an effort to help their furry friend live their best life. My team and I were able to design and implement a RESTful API using Spring Boot with full CRUD functionality that communicates with the frontend to authenticate with JWTs.</p>
 
 
             {/*</div>*/}
             {/*<div>*/}
             {/*    <h2 className="h-20">About Us.</h2>*/}
-            {/*    <p className="w-50">A full-stack web application that allows you to find your best buddy, a play date. It’s complete with a modern design made using React.js, styled with TailwindCSS, animated with Framer Motion. The user can sign up for an account then get to creating their dog’s profile. It allows dog-owners to collaborate in an effort to help their furry friend live their best life. My team and I were able to design and implement a RESTful API using Spring Boot with full CRUD functionality that communicates with the frontend to authenticate with JWTs.</p>*/}
+            {/*    <p className="w-50">A full-stack web application that allows you to find your best buddy, a play date. It's complete with a modern design made using React.js, styled with TailwindCSS, animated with Framer Motion. The user can sign up for an account then get to creating their dog's profile. It allows dog-owners to collaborate in an effort to help their furry friend live their best life. My team and I were able to design and implement a RESTful API using Spring Boot with full CRUD functionality that communicates with the frontend to authenticate with JWTs.</p>*/}
 
             {/*</div>*/}
             <div className={`flex gap-1 flex-wrap justify-center`}>
