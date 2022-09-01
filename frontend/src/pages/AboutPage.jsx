debba078712647447a19656976f8ebd5c00dc55a
Alex Rodriguez <alexander.joshua.rodriguez@gmail.com>
2022-09-01 15:04:23 -0500

    update to about-us

--- frontend/src/pages/AboutPage.jsx
+++ frontend/src/pages/AboutPage.jsx
@@ -6,42 +6,52 @@ const AboutPage = () => {
 
     return (
         <motion.main {...pt} className={`p-4`}>
-            <div>
-                <h2 className="h-20">About Us.</h2>
-                <p className="w-50">A full-stack web application that allows you to find your best buddy, a play date. It’s complete with a modern design made using React.js, styled with TailwindCSS, animated with Framer Motion. The user can sign up for an account then get to creating their dog’s profile. It allows dog-owners to collaborate in an effort to help their furry friend live their best life. My team and I were able to design and implement a RESTful API using Spring Boot with full CRUD functionality that communicates with the frontend to authenticate with JWTs.</p>
+            {/*<div className="hero min-h-screen" style="background-image: url(https://placeimg.com/1000/800/arch);">*/}
+                <div className="hero-overlay bg-opacity-60"></div>
+                <div className="hero-content text-center text-neutral-content">
+                    <div className="max-w-md">
+                        <h1 className="mb-5 text-5xl font-bold">About Us.</h1>
+                        <p className="mb-5">A full-stack web application that allows you to find your best buddy, a play date. It’s complete with a modern design made using React.js, styled with TailwindCSS, animated with Framer Motion. The user can sign up for an account then get to creating their dog’s profile. It allows dog-owners to collaborate in an effort to help their furry friend live their best life. My team and I were able to design and implement a RESTful API using Spring Boot with full CRUD functionality that communicates with the frontend to authenticate with JWTs.</p>
+                    </div>
+                </div>
+            {/*</div>*/}
+            {/*<div>*/}
+            {/*    <h2 className="h-20">About Us.</h2>*/}
+            {/*    <p className="w-50">A full-stack web application that allows you to find your best buddy, a play date. It’s complete with a modern design made using React.js, styled with TailwindCSS, animated with Framer Motion. The user can sign up for an account then get to creating their dog’s profile. It allows dog-owners to collaborate in an effort to help their furry friend live their best life. My team and I were able to design and implement a RESTful API using Spring Boot with full CRUD functionality that communicates with the frontend to authenticate with JWTs.</p>*/}
 
-            </div>
+            {/*</div>*/}
             <div className={`flex gap-1 flex-wrap justify-center`}>
                 <AboutCard
-                    img="../public/img/D. Miller.jpg"
+                    img="../img/D. Miller.jpg"
                     name="Dalton"
                     bio="Would you like me to park your car?"
                     linkedIn="https://www.linkedin.com/in/daltonkyemiller/"
                     githubLink="https://github.com/daltonkyemiller"
                 />
+
                 <AboutCard
-                    img="../public/img/C. Gallagos.jpg"
+                    img="../img/C. Gallagos.jpg"
                     name="Cynthia"
                     bio="Bring me borgar"
                     linkedIn="https://www.linkedin.com/in/cynthialgallegos/"
                     githubLink="https://github.com/CGALLE39"
                 />
                 <AboutCard
-                    img="../public/img/M. Jio.jpg"
+                    img="../img/M. Jio.jpg"
                     name="Matt"
                     bio="Here in my car.."
                     linkedIn="https://www.linkedin.com/in/matt-jio/"
                     githubLink="https://github.com/mattjio"
                 />
                 <AboutCard
-                    img="../public/img/J. Welsh.jpg"
+                    img="../img/J. Welsh.jpg"
                     name="Justin"
                     bio="Very nice!"
                     linkedIn="https://www.linkedin.com/in/justin-welsh/"
                     githubLink="https://github.com/JustinWelsh"
                 />
                 <AboutCard
-                    img="../public/img/A. Rodriguez.jpg"
+                    img="../img/A. Rodriguez.jpg"
                     name="Alex"
                     bio="Oi, you gotta get on this TRT bro !"
                     linkedIn="www.linkedin.com/in/alexanderjrodriguez"
