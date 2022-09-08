c2a58c93cc442748de507ace924662d270cfaa77
Justin Welsh <justinwelsh33@gmail.com>
2022-09-08 09:16:42 -0500

    fixed conflict

--- frontend/src/pages/DogProfile.jsx
+++ frontend/src/pages/DogProfile.jsx
@@ -143,7 +143,7 @@ const DogProfile = () => {
                                         src={
                                             dog?.owner?.img
                                                 ? `${FILESTACK_ENDPOINT}/${dog.owner.img}`
-                                                : '/img/placholder-img.jpeg'
+                                                : '../public/img/placholder-img.jpeg'
                                         }
                                         alt="User pic"
                                         className={`rounded-full w-full h-full object-cover`}
